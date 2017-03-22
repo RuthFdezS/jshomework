3d0453b5969d43def25afad038b254d92a5491fd
mariaastorgam <maria.astorga@accenture.com>
2017-03-21 19:14:17 -0600

    remove the double item

--- homework1/bower_components/jquery/dist/imageplaypause.js
+++ homework1/bower_components/jquery/dist/imageplaypause.js
@@ -6,12 +6,15 @@ $(document).ready(function (){
  $('#my-video-player').click(function() { 
     if(this.paused){
 		this.play(); 
+		$('#icon-play').remove();
 		$('#play-control').removeClass('fa fa-play');
 		$('#play-control').addClass('fa fa-pause');
 	} else if(this.play){
 			this.pause(); 
+			$('#icon-play').remove();
 			$('#play-control').removeClass('fa fa-pause');
 			$('#play-control').addClass('fa fa-play');
+			
 		}
 	});	
 });
