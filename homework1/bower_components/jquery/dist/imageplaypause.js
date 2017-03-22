639513fb73be3aa400090bee643a32caf6c87fbd
mariaastorgam <maria.astorga@accenture.com>
2017-03-21 19:07:50 -0600

    stop imageplaypause

--- homework1/bower_components/jquery/dist/imageplaypause.js
+++ homework1/bower_components/jquery/dist/imageplaypause.js
@@ -1,5 +1,17 @@
 $(document).ready(function (){
-$('#my-video-player').click(function() {
-        this.paused ? this.play() : this.pause();
-    });
+//$('#my-video-player').click(function() {
+//        this.paused ? this.play() : this.pause();
+//    });
+
+ $('#my-video-player').click(function() { 
+    if(this.paused){
+		this.play(); 
+		$('#play-control').removeClass('fa fa-play');
+		$('#play-control').addClass('fa fa-pause');
+	} else if(this.play){
+			this.pause(); 
+			$('#play-control').removeClass('fa fa-pause');
+			$('#play-control').addClass('fa fa-play');
+		}
+	});	
 });
