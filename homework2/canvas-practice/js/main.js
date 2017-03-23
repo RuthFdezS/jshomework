848d2667adf5821d356da6c12da8576c4f6d59f0
snakedr999 <gsotto91@gmail.com>
2017-03-23 12:15:57 -0600

    I has added new content in main.js and index.html

---
+++ homework2/canvas-practice/js/main.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+	init();
+};
+
+var contenedor, canvas, context, imageObj;
+var texto = document.getElementById('alert');
+
+function init() {
+
+	 	contenedor = document.getElementById('container');
+
+		if (Modernizr.canvas) {
+
+			texto.innerHTML = 'Your browser support HTML5 canvas';
+
+			canvas = document.createElement('canvas');
+			
+			context = canvas.getContext('2d');
+			context.clearRect(0,0,720,300);
+			
+			var sky = context.createLinearGradient(0,0,0,150);
+			sky.addColorStop(0, '#001848');
+			sky.addColorStop(1, '#301860');
+
+			context.fillStyle = sky;
+			context.fillRect(0,0,720,300);
+
+			contenedor.appendChild(canvas);
+
+			crearImagen();
+			dibujarImagen();
+
+			
+		} else {
+
+			texto.innerHTML = ':Your browser does not support HTML5 canvas ';
+			fallBackImage();
+		
+		}	
+};
+
+function crearImagen() {
+
+	var _self = this;
+
+	imageObj = new Image();
+	imageObj.src = "assets/mountains.png";
+	
+	imageObj.onload = function() {
+		_self.dibujarImagen();
+	}
+}
+
+function dibujarImagen() {
+
+	context.save();
+	context.scale(0.4, 0.4);
+	context.drawImage(imageObj, 0, 75, 750, 300);
+	context.restore();
+}
+
+function fallBackImage() {
+
+	fallBackImage = document.createElement('img');
+	fallBackImage.setAttribute('src', "assets/night.jpg");
+	contenedor.appendChild(fallBackImage);
+
+}
+
+
+
